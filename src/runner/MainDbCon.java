30e970647db54eb0290f49fa441bc0d163def8be
unknown <MohamedSondo>
2018-11-13 13:26:59 -0800

    Welcome Page done

--- src/runner/MainDbCon.java
+++ src/runner/MainDbCon.java
@@ -9,6 +9,75 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class MainDbCon {
+	
+	public static void transMenu() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException, IOException {
+		System.out.println("Welcome to the Transaction Menu\n");
+		System.out.println("Please select from options below\n");
+
+		
+		System.out.println("1. View transaction by Zipcode\n");
+		System.out.println("2. View totals by Transaction Type\n");
+		System.out.println("3. View totals by State \n");
+		Scanner sc = new Scanner(System.in);
+		int transMenuChoice = sc.nextInt();
+		CRUD transObj = new CRUD();
+		switch(transMenuChoice) {
+
+		case 1 :transObj.selectTrans1();
+			break;
+		
+		case 2 :transObj.selectTrans2();
+			break;
+		case 3 :transObj.selectTrans3();
+			break;
+			
+
+		default :
+			System.out.println("Invalid enter, please refresh the page\n");
+		
+		}
+
+		
+		
+	}
+	
+	
+	public static void custMenu() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException, IOException {
+		System.out.println("Welcome to the Customer Details Menu\n");
+		System.out.println("Please select from options below\n");
+		System.out.println("1. Check existing account details of a customer\n");
+		System.out.println("2. Modify existing account details of a customer\n");
+		System.out.println("3. Generate mothly bill for a credit card number for a given month and year\n");
+		System.out.println("4. Display transactions made by a customer between two dates\n");
+		//System.out.println("Make a choice: ");
+		
+		Scanner sc = new Scanner(System.in);
+		int custMenuChoice = sc.nextInt();
+		
+
+		CRUD custObj = new CRUD();
+		
+		
+		switch(custMenuChoice) {
+		
+		case 1 :custObj.selectTrans21();
+			break;
+		
+		case 2 :custObj.update();
+			break;
+		case 3 :custObj.selectTrans23();
+			break;
+			
+		case 4 :custObj.selectTrans24();
+			break;
+			
+		default :
+			System.out.println("Invalid enter, please refresh the page\n");
+		}
+		
+		
+		
+	}
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, IOException {
 		
@@ -20,114 +89,29 @@ public class MainDbCon {
 //		System.out.println("If you wish details of transaction, press 1; if you wish details of customer, press 2: ");
 //		Scanner sc1 = new Scanner(System.in);
 //		int response1 = sc1.nextInt();
-//		
-//		
-//		switch(response1) {
-//		// case for transactions
-//		case 1: 
-//			
-//			Transaction tract = new Transaction();
-//			sc1.nextLine();
-//			System.out.println("1. View transaction by Zipcode");
-//			tract.getZipcode();
-//			System.out.println("2. View totals by Transaction Type");
-//			tract.getType();
-//			System.out.println("3. View totals by State");
-//			tract.getState();
-//			System.out.println("Make a choice: ");
-//			Scanner sc2 = new Scanner(System.in);
-//			int response2 = sc2.nextInt();
-//			
-//			switch(response2) {
-//			case 1: 
-//				
-//				
-//				
-//				break;
-//				
-//			case 2: 
-//				
-//				
-//				break;
-//				
-//				
-//			case 3: 
-//				
-//				
-//				break;
-//		
-//			
-//			default: 
-//			System.out.println("Invalid enter, please refresh the page");
-//			}
-//			
-//			case 2: 
-//			
-//			System.out.println("1. Check existing account details of a customer");
-//			tract.getCount();
-//			System.out.println("2. Modify existing account details of a customer");
-//			tract.setCount(count);
-//			System.out.println("3. Generate mothly bill for a credit card number for a given month and year");
-//			////// create a row
-//			System.out.println("4. Display transactions made by a customer between two dates");
-//			tract.getTransaction();
-//			System.out.println("Make a choice: ");
-//			
-//			break;
-//			
-//			Scanner sc3 = new Scanner(System.in);
-//			int response3 = sc3.nextInt();
-//			
-//			switch(response3) {
-//			case 1: 
-//				
-//				
-//				break;
-//				
-//			case 2: 
-//				
-//				
-//				break;
-//				
-//				
-//			case 3: 
-//				
-//				
-//				break;
-//
-//			case 4: 
-//				
-//				
-//				break;
-//
-//				
-//			default: 
-//				System.out.println("Invalid enter, please refresh the page");
-//
-//			}
-//			
-//		default: 
-//			System.out.println("Invalid enter, please refresh the page");
-//
-//			
-//		}
 		
+	while(true) {	
+		System.out.println("Welcome to the Credit Card Management System:\n ");
+		System.out.println("What function do you want to perform today?\n\n"
+				+ "1. Transaction Detail \n"
+				+ "2. Customer detail\n ");
+	
+	Scanner sc1 = new Scanner(System.in);
+		int funcChoice = sc1.nextInt();
+		switch(funcChoice) {
 		
-		
-			CRUD sec = new CRUD();
-			sec.selectTrans24();
-//			
-//			
-		
-//			CRUD cre = new CRUD();
-//			cre.create();
-//			
-//			CRUD ins = new CRUD();
-//			ins.insert();
-//			
-//			CRUD upd = new CRUD();
-//			upd.update();
+		case 1:transMenu();
+			break;
+		case 2:custMenu();
+			break;
+		default:
+		System.out.println("Wrong selection or invalid input\n");
 			
+		}
+	}
+		
+		
+				
 		}
 	}
 
